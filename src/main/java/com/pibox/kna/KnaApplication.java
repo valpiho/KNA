26c9a93b7ada9680db5f4e568600715c190d28cc
Valentin Piho <val.piho@gmail.com>
2021-05-27 17:35:11 +0300

    Init

---
+++ src/main/java/com/pibox/kna/KnaApplication.java
@@ -0,0 +1,13 @@
+package com.pibox.kna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pibox/kna/KnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pibox.kna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
