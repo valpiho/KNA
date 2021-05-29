15f5b2662cc1555ea5de079a5224806cdb65d480
Yury <ryv8@tut.by>
2021-05-29 18:37:15 +0300

    KNA-5: getAllOrders, addNewOrder

--- src/main/java/com/pibox/kna/domain/Order.java
+++ src/main/java/com/pibox/kna/domain/Order.java
@@ -2,10 +2,14 @@ package com.pibox.kna.domain;
 
 import com.pibox.kna.domain.Enumeration.Status;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
 
+@Getter
+@Setter
 @Data
 @Entity
 @Table(name = "kna_order")
--- src/main/java/com/pibox/kna/service/impl/OrderServiceImpl.java
+++ src/main/java/com/pibox/kna/service/impl/OrderServiceImpl.java
@@ -1,8 +1,25 @@
 package com.pibox.kna.service.impl;
 
+import com.pibox.kna.domain.Enumeration.Status;
+import com.pibox.kna.domain.Order;
+import com.pibox.kna.repository.OrderRepository;
 import com.pibox.kna.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+
+    public Order addNewOrder(Order order){
+        return orderRepository.save(order);
+    }
 }
