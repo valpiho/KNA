c6092b4cd4f916eb7faa903883973cc2533a0e2a
Valentin Piho <val.piho@gmail.com>
2021-06-04 14:55:17 +0300

    Integration tests update 'Happy Paths'

--- src/test/java/com/pibox/kna/web/rest/AbstractTestIT.java
+++ src/test/java/com/pibox/kna/web/rest/AbstractTestIT.java
@@ -22,8 +22,7 @@ import javax.transaction.Transactional;
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
-import static com.pibox.kna.domain.Enumeration.Role.ROLE_CLIENT;
-import static com.pibox.kna.domain.Enumeration.Role.ROLE_DRIVER;
+import static com.pibox.kna.domain.Enumeration.Role.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
@@ -36,10 +35,10 @@ public class AbstractTestIT {
     protected MockMvc mockMvc;
 
     @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
+    protected BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    private UserRepository userRepository;
+    protected UserRepository userRepository;
 
     UserDTO userDtoAsDriver= UserDTO.builder()
             .firstName("John").lastName("Doe")
@@ -58,34 +57,47 @@ public class AbstractTestIT {
 
     User user1;
     User user2;
+    User admin;
 
     @BeforeEach
     void setUp() {
         user1 = new User();
+        user1.setFirstName("Val");
+        user1.setLastName("Piho");
         user1.setUsername("valPiho");
         user1.setEmail("val.piho@ail.com");
         user1.setPassword(bCryptPasswordEncoder.encode("pq1Ax0k1BW"));
         user1.setActive(true);
-        user1.setImageUrl("http://localhost:8080/api/v1/image/val2");
+        user1.setImageUrl("http://localhost:8080/api/v1/image/valPiho");
         user1.setJoinDate(new Date());
         user1.setRole(ROLE_DRIVER.name());
         user1.setAuthorities(ROLE_DRIVER.getAuthorities());
         user1.setDriver(new Driver("3232"));
 
         user2 = new User();
+        user1.setFirstName("Alex");
+        user1.setLastName("Piho");
         user2.setUsername("alexPiho");
         user2.setEmail("alex.piho@ail.com");
         user2.setPassword(bCryptPasswordEncoder.encode("Y65QXnY6fP"));
         user2.setActive(true);
-        user2.setImageUrl("http://localhost:8080/api/v1/image/val2");
+        user2.setImageUrl("http://localhost:8080/api/v1/image/alexPiho");
         user2.setJoinDate(new Date());
         user2.setRole(ROLE_CLIENT.name());
         user2.setAuthorities(ROLE_CLIENT.getAuthorities());
         user2.setClient(new Client("company@ail.com", "34234234234", "Estonia", "Tallinn", "Liivalaia 13", "42342"));
 
+        admin = new User();
+        admin.setUsername("admin");
+        admin.setEmail("admin@ail.com");
+        admin.setPassword(bCryptPasswordEncoder.encode("GcyeBV6xPx"));
+        admin.setActive(true);
+        admin.setRole(ROLE_ADMIN.name());
+        admin.setAuthorities(ROLE_ADMIN.getAuthorities());
 
         userRepository.save(user1);
         userRepository.save(user2);
+        userRepository.save(admin);
     }
 
     protected ResultActions login(String username, String password) throws Exception {
--- src/test/java/com/pibox/kna/web/rest/UserResourceTestIT.java
+++ src/test/java/com/pibox/kna/web/rest/UserResourceTestIT.java
@@ -1,5 +1,6 @@
 package com.pibox.kna.web.rest;
 
+import com.pibox.kna.service.dto.UserDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
@@ -13,37 +14,48 @@ class UserResourceTestIT extends AbstractTestIT{
 
     @Test
     public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
-        mockMvc.perform(get("/api/v1/testURL"))
-                .andExpect(status().isForbidden());
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/testURL"));
+
+        resultActions.andExpect(status().isForbidden());
+    }
+
+    @Test
+    void getUserAvatar() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/image/alexPiho"));
+
+        resultActions.andExpect(status().isOk());
     }
 
     @Test
     void registerUserAsDriver() throws Exception {
-        mockMvc.perform(post("/api/v1/register")
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(userDtoAsDriver)))
-                .andExpect(status().isCreated())
+                .content(asJsonString(userDtoAsDriver)));
+
+        resultActions.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.message", is("User has been registered successfully")));
     }
 
     @Test
     void registerUserAsClient() throws Exception {
-        mockMvc.perform(post("/api/v1/register")
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(userDtoAsClient)))
-                .andExpect(status().isCreated())
+                .content(asJsonString(userDtoAsClient)));
+
+        resultActions.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.message", is("User has been registered successfully")));
     }
 
     @Test
     void userCanResetPassword() throws Exception {
-        mockMvc.perform(get("/api/v1/reset-password/val.piho@ail.com"))
-                .andExpect(status().isOk())
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/reset-password/val.piho@ail.com"));
+
+        resultActions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.message", is("An email with a new password was sent to: val.piho@ail.com")));
     }
 
     @Test
-    public void userCanToUsersList() throws Exception {
+    public void userCanAccessToUsersList() throws Exception {
         final String token = extractToken(login(user1.getUsername(), "pq1Ax0k1BW").andReturn());
         ResultActions resultActions = mockMvc.perform(get("/api/v1/users")
                 .header("Authorization", "Bearer " + token));
@@ -51,30 +63,64 @@ class UserResourceTestIT extends AbstractTestIT{
         resultActions.andExpect(status().isOk());
     }
 
-
-
-
     @Test
-    void getAllUsers() {
+    public void userCanAccessToAnotherUserAsDriverAccount() throws Exception {
+        final String token = extractToken(login(admin.getUsername(), "GcyeBV6xPx").andReturn());
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/users/valPiho")
+                .header("Authorization", "Bearer " + token));
+
+        resultActions.andExpect(status().isOk());
     }
 
     @Test
-    void getUserByUsername() {
+    public void userCanAccessToAnotherUserAsClientAccount() throws Exception {
+        final String token = extractToken(login(admin.getUsername(), "GcyeBV6xPx").andReturn());
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/users/alexPiho")
+                .header("Authorization", "Bearer " + token));
+
+        resultActions.andExpect(status().isOk());
     }
 
     @Test
-    void addNewUser() {
+    public void userAsAdminCanAddNewUser() throws Exception {
+        final String token = extractToken(login(admin.getUsername(), "GcyeBV6xPx").andReturn());
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/users")
+                .header("Authorization", "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(userDtoAsClient)));
+
+        resultActions.andExpect(status().isCreated());
     }
 
     @Test
-    void updateUser() {
+    public void userAsAdminCanUpdateUserAccount() throws Exception {
+        UserDTO user = UserDTO.builder()
+                .firstName("Alex")
+                .lastName("Piho")
+                .username("alexPiho")
+                .email("alex@ail.com")
+                .clientEmail("companyX@ail.com")
+                .clientPhoneNumber("423423")
+                .clientCountry("Latvia")
+                .clientCity("Riga")
+                .clientStreetAddress("Lotusa 13")
+                .clientZipCode("12333")
+                .build();
+        final String token = extractToken(login(admin.getUsername(), "GcyeBV6xPx").andReturn());
+        ResultActions resultActions = mockMvc.perform(patch("/api/v1/users/alexPiho")
+                .header("Authorization", "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(user)));
+
+        resultActions.andExpect(status().isOk());
     }
 
     @Test
-    void deleteUser() {
-    }
+    public void userAsAdminCanDeleteUser() throws Exception {
+        final String token = extractToken(login(admin.getUsername(), "GcyeBV6xPx").andReturn());
+        ResultActions resultActions = mockMvc.perform(delete("/api/v1/users/alexPiho")
+                .header("Authorization", "Bearer " + token));
 
-    @Test
-    void getTempProfileImage() {
+        resultActions.andExpect(status().isOk());
     }
 }
